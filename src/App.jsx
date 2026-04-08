be35e7080d5077b1daaca51db42a81ad0f2bc001
Your Name <you@example.com>
2026-04-08 17:05:59 +0200

    5.2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import blogService from './services/blogs'
 import Login from './components/Login'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import './index.css'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -20,15 +21,33 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
+    console.log('logging in with', username, password)
     try {
       const user = await loginService.login({ username, password })
-      console.log(user)
       blogService.setToken(user.token)
+      window.localStorage.setItem(
+        'loggedBloglistUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
+      setErrorMessage('login successful')
+      setClassName('success')
+      setTimeout(() => {
+        setErrorMessage(null)
+        setClassName('')
+      }, 5000)
     } catch {
       setErrorMessage('wrong credentials')
       setClassName('error')
@@ -37,6 +56,12 @@ const App = () => {
         setClassName('')
       }, 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBloglistUser')
+    setUser(null)
+    blogService.setToken(null)
   }
 
   return (
@@ -55,7 +80,7 @@ const App = () => {
         />
       ) : (
         <div>
-          <p>{user.name} logged in</p>
+          <p>{user.name} logged in <button onClick={handleLogout}>Logout</button></p>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
